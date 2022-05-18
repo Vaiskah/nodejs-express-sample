5f621cb7144e94945873bbc8d6b728b5169df224
Vaiskah <kichuvaisakh@gmail.com>
2022-05-18 16:20:34 +0530

    router added

---
+++ route/auth.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.post("/", (req, res) => {
+    console.log(req.body);
+    const { name } = req.body;
+    res.send(`Welcome ${name}`);
+  });
+
+  module.exports = router
