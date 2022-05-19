4e63f5ba54d1be17e281832306ec49c0e09c4159
Vaiskah <kichuvaisakh@gmail.com>
2022-05-19 11:35:42 +0530

    controller folder and peopleRouter added-simplify people

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 const express = require("express");
 
 const app = express();
-const people = require('./route/people')
-const auth = require('./route/auth')
-
+const people = require("./route/people");
+const auth = require("./route/auth");
 
 app.use(express.static("./methods-public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/api/people', people)
+app.use("/api/people", people);
 
-app.use('/login', auth)
-
-
-
-
+app.use("/login", auth);
 
 app.listen(5000, () => {
   console.log("server is listening port 5000......");
--- route/auth.js
+++ route/auth.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 router.post("/", (req, res) => {
-    console.log(req.body);
-    const { name } = req.body;
-    res.send(`Welcome ${name}`);
-  });
+  console.log(req.body);
+  const { name } = req.body;
+  res.send(`Welcome ${name}`);
+});
 
-  module.exports = router
+module.exports = router;
